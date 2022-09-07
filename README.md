5239383ceddec1ff638cb1dd8b80a1b5f714aff0
daviesayoo <73048228+daviesayoo@users.noreply.github.com>
2022-09-07 12:20:06 -0500

    make it better

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# flappy-bird-game
+# Flappy-Bird-Game
+
+A Flappy Bird game made using JavaScript and the p5 library with help from Daniel Schiffman's videos. The most basic version of the game
--- server.tsx
+++ server.tsx
@@ -5,11 +5,11 @@ const router = express.Router();
 
 app.use(express.static(path.join(__dirname, '/')));
 
-router.get('/', function(req, res){
+app.get('/', function(req, res){
 	res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 app.use('/', router);
-app.listen(process.env.port || 3000);
+app.listen(process.env.port || 8080);
 
-console.log('Listening on port 3000');
+console.log('Listening on port 8080');
