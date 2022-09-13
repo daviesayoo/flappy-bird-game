32873a275ce958f43c87fa656e02b7ada9455c69
daviesayoo <73048228+daviesayoo@users.noreply.github.com>
2022-09-13 14:33:58 -0500

    add start scripts

--- server.tsx
+++ server.tsx
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const router = express.Router();
+const compression = require('compression');
+
+
+
+// app.use(secure);
+app.disable('etag');  //this for a change of StatusCode 304 to 200 OK
+app.use(compression());
+
 
 app.use(express.static(path.join(__dirname, '/')));
 
@@ -9,7 +17,10 @@ app.get('/', function(req, res){
 	res.sendFile(path.join(__dirname, '/index.html'));
 });
 
+const PORT = process.env.PORT || 8080;
 app.use('/', router);
-app.listen(process.env.port || 8080);
 
+app.listen(PORT, function(err) {
+	if (err) console.log(err);
 console.log('Listening on port 8080');
+});
