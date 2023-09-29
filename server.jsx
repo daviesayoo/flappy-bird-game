13056066442102890d007e8f2ecf8ccd102d9f80
daviesayoo <73048228+daviesayoo@users.noreply.github.com>
2023-09-29 14:43:02 -0500

    add start scripts

--- server.jsx
+++ server.jsx
@@ -17,7 +17,7 @@ app.get('/', function(req, res){
 	res.sendFile(path.join(__dirname, '/index.html'));
 });
 
-const PORT = process.env.PORT || 8443;
+const PORT = process.env.PORT || 8080;
 app.use('/', router);
 
 app.listen(PORT, function(err) {
